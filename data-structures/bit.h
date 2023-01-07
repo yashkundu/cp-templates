af643bfe44d178005f9b0fe716d97f75b0517f4e
yashkundu <yashasvik107@gmail.com>
2023-01-08 01:06:22 +0530

    commit

---
+++ data-structures/bit.h
@@ -0,0 +1,30 @@
+/**
+*    author:  lazyhash(yashkundu)
+*    created: 08 Jan,2023
+*    description: compute partial sums (a[0]+a[1]... + a[k-1]) and does point updates a[i]
+*    time: add, get takes O(logn)
+**/
+
+template<typename T>
+struct bit{
+    int n;
+    vector<T> v;
+    bit(int n): n(n) {v.resize(n+1);}
+    add(int ind, T val) {
+        ind++; for(;ind<n;ind+=ind&-ind) v[ind] += val;
+    }
+    T get(int ind) {
+        ind++; T ans = T(); 
+        for(;ind>0;ind-=ind&-ind) ans += v[ind]; return ans; 
+    }
+    T get(int l, int r) {
+        return get(r+1) - get(l);
+    }
+    T lower_bound(T x) {
+        int ind = 0;
+        for(int pw=1<<25;pw>0;pw>>=1) {
+            if(ind+pw<=n && sum+v[ind+pw]<x) ind = ind +  pw, x -= v[ind+pw];
+        }
+        return ind;
+    }
+};
