4a01532fc5e3f99203bf390d886a2eead5870ae0
Yashasvi Kundu <yashkundu@Yashasvis-MacBook-Pro.local>
2024-05-09 11:55:25 +0530

    added ftree

--- data-structures/Bit.cpp
+++ data-structures/Bit.cpp
@@ -1,28 +1,39 @@
-/**
-*   author:  lazyhash(yashkundu)
-*   created: 08 Jan,2023
-*   description: compute partial sums (a[0]+a[1]... + a[k-1]) and does point updates a[i]
-*   time: add, get takes O(logn)
-**/
+// Verification: https://codeforces.com/problemset/submission/1181/260078595
 
-template<typename T>
-struct bit{
+#include <iostream>
+#include <vector>
+#include <random>
+#include <chrono>
+ 
+using namespace std;
+
+const int M = 18;
+ 
+struct BIT{
     int n;
-    vector<T> v;
-    bit(int n): n(n) {v.resize(n+1);}ß
-    void add(int ind, T val) {
-        ind++; for(;ind<=n;ind+=ind&(-ind)) v[ind] += val;
+    vector<int> tree;
+    BIT(int n) {
+        tree.resize(n+1);
+        tree.assign(n+1, 0);
+        this->n = n;
     }
-    T get(int ind) {
-        ind++; T ans = T(); 
-        for(;ind>0;ind-=ind&(-ind)) ans += v[ind]; return ans; 
+    void update(int ind, int val) {
+        for(;ind<=n;ind+=ind&(-ind)) tree[ind] += val;
     }
-    T get(int l, int r) {
-        return get(r) - get(l-1);
+    int query(int ind) {
+        int ans = 0;
+        for(;ind>0;ind-=ind&(-ind)) ans += tree[ind];
+        return ans;
     }
-    T lower_bound(T x) {
-        int ind = 0, sum = T();
-        for(int pw=1<<25;pw>0;pw>>=1) if(ind+pw<=n && sum + v[ind+pw]<x) ind = ind +  pw, sum += v[ind+pw];
-        return ind;
+    int lower_bound(int x) {
+        int ind = 0;
+        int sum = 0;
+        for(int p=(1<<M);p>0;p>>=1) {
+            if(ind+p<=n && sum + tree[ind+p]<x) {
+                ind = ind + p;
+                sum += tree[ind];
+            }
+        }
+        return ind+1;
     }
 };
